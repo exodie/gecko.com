e94cb7acee9724ba27081282fc33a17e16d855bd
im. <83586607+exodie@users.noreply.github.com>
2024-04-13 15:05:46 +0400

    fix: add mobile resizable

--- src/widgets/header/index.tsx
+++ src/widgets/header/index.tsx
@@ -1,11 +1,24 @@
 import { LogoOfHeader, Navbar } from "./ui";
 
+const ChildrenOfHeader = () => {
+  return (
+    <>
+      <LogoOfHeader />
+      <Navbar />
+    </>
+  );
+};
+
 export const Header = () => {
   return (
-    <header className="flex flex-row items-center justify-between px-12 py-4">
-      <LogoOfHeader />
+    <header className="lg:px-12 py-4">
+      <div className="lg:flex hidden flex-row items-center justify-center lg:justify-between">
+        <ChildrenOfHeader />
+      </div>
 
-      <Navbar />
+      <div className="lg:hidden flex flex-col items-center gap-y-4">
+        <ChildrenOfHeader />
+      </div>
     </header>
   );
 };
